ae3df7fb2e53cfdef5e8762b2c46b20acb021bb0
Shogo Matsumoto <sougyo2001@gmail.com>
2024-05-02 11:01:08 +0000

    add index.html

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -10,9 +12,17 @@ func Add(a, b int) int {
 }
 
 func main() {
+	filePath := "index.html"
+
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("err readfile")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!!!")
+		fmt.Fprintf(w, string(content))
 	})
+
 	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
