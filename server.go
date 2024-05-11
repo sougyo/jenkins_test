15eca244c5a21b69c0e57e846e706a111fe1dfb3
Shogo Matsumoto <sougyo2001@gmail.com>
2024-05-11 21:14:32 +0000

    connect server and client javascript

--- server.go
+++ server.go
@@ -21,6 +21,18 @@ func main() {
 			log.Fatalf("err writing response: %v", err)
 		}
 	})
+	http.HandleFunc("/move", func(w http.ResponseWriter, r *http.Request) {
+		_, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = w.Write([]byte("OK"))
+		if err != nil {
+			http.Error(w, "Failed to write request body", http.StatusInternalServerError)
+		}
+	})
 
 	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", nil)
